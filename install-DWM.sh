d15a46a56c235de2f3fe98a7a72376a5f21533ab
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2023-10-29 03:06:38 +0100

    Update install-DWM.sh

--- install-DWM.sh
+++ install-DWM.sh
@@ -145,53 +145,6 @@ sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
 echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" | sudo tee -a /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-#Asus-Linux.org stuff
-
-titel_message_length=${#titel_message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
-
-message="Adding repo from Asus-Linux.org"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-#Add repo
-sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-wget "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x8b15a6b0e9a3fa35" -O g14.sec
-sudo pacman-key -a g14.sec
-sudo rm g14.sec
-echo "[g14]
-Server = https://arch.asus-linux.org" | sudo tee -a /etc/pacman.conf
-sudo pacman -Suy
-
-
-titel_message_length=${#titel_message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
-
-message="Installing from Asus-Linux.org"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-message2="Asusctl, Supergfxctl, ROG Control Center"
-message2_length=${#message2}
-spaces=$(( (${#separator} - message2_length) / 2 ))
- printf "%s%${spaces}s%s\n%s\n" "" "" "$message2" "$separator"
-
-#Installing
-sudo pacman -Sy --noconfirm --needed asusctl supergfxctl rog-control-center
-
-#Asusctl - custom fan profiles, anime, led control etc
-sudo systemctl enable --now power-profiles-daemon.service
-
-#Supergfxctl - graphics switching
-sudo systemctl enable --now supergfxd
-
 startx
 
 
