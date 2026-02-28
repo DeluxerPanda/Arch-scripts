7179241521eb68734aee016fa95da66f930adee6
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2026-02-28 14:13:25 +0100

    Update setup_DWM_for_Arch.sh

--- setup_DWM_for_Arch.sh
+++ setup_DWM_for_Arch.sh
@@ -70,7 +70,7 @@ function Installing() {
     sudo pacman -Syu --noconfirm
 
     # System packages
-    sudo pacman -S --needed --noconfirm base-devel libx11 libxft xorg-server xorg-xinit ffmpeg networkmanager mate-polkit nfs-utils nano usbutils gnome-keyring fuse
+    sudo pacman -S --needed --noconfirm base-devel libx11 libxft xorg-server xorg-xinit ffmpeg networkmanager network-manager-applet mate-polkit nfs-utils nano usbutils gnome-keyring fuse
 
     # Fonts
     sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd noto-fonts-emoji
