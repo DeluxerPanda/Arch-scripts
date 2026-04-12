29b95b4500629338de4dd053fc321bdf993e87da
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2026-04-12 19:03:10 +0200

    update

--- Arch-install.sh
+++ Arch-install.sh
@@ -637,7 +637,7 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 
-pacman -S --noconfirm --needed bash-completion nfs-utils usbutils nano bat ffmpeg btop gnome-keyring fuse pipewire dunst starship
+pacman -S --noconfirm --needed bash-completion nfs-utils usbutils nano bat ffmpeg btop gnome-keyring fuse pipewire dunst starship fastfetch
 pacman -S --noconfirm --needed pavucontrol sddm dolphin
 pacman -S --noconfirm --needed steam gamescope prismlauncher
 pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd noto-fonts-emoji
