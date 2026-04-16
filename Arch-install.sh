1048f2d88f62c0c43ec18585f5bde55af1f00a9d
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2026-04-16 17:14:28 +0200

    Update Arch-install.sh

--- Arch-install.sh
+++ Arch-install.sh
@@ -756,6 +756,11 @@ cd /home/$USERNAME
 
     mkdir -p /home/$USERNAME/Bilder/backgrounds
     wget -O /home/$USERNAME/Bilder/backgrounds/wallpaper.jpg "https://lh3.googleusercontent.com/pw/AP1GczNr22gSNbdSNq_08trKdHkkswDq1k2PuefBqriaPp86lshFr10RjFqKQ_phn0187riksWgh-ouqn_6-MkHwVb5nIpyCaiH34WCOIywCis8X39gV3q3Fsy_9HZO-he7gxYnjbt7zulTazkiIj4qxyBjY"
+
+    wget -O /home/$USERNAME/.config/rofi/config.rasi "https://raw.githubusercontent.com/DeluxerPanda/Arch-scripts/main/config/rofi/config.rasi"
+    mkdir -p /home/$USERNAME/.config/rofi/themes
+    wget -O /home/$USERNAME/.config/rofi/themes/catppuccin-mocha.rasi "https://raw.githubusercontent.com/DeluxerPanda/Arch-scripts/main/config/rofi/themes/catppuccin-mocha.rasi"
+    
 "
     touch /etc/environment
     cat >> /etc/environment <<'EOF'
