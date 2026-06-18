8249e1332d0e77069acbc66551e070593cd842c1
DeluxerPanda <kontakt@deluxerpanda>
2026-06-18 18:56:44 +0200

    update

--- Arch-install.sh
+++ Arch-install.sh
@@ -216,7 +216,7 @@ userinfo () {
     done
     export NAME_OF_MACHINE=$name_of_machine
 }
-
+export ENV="Env_Kde"
 setupEnv () {
         echo -ne "  
     -----------------------------------------------------------------------
@@ -376,7 +376,7 @@ logo
 userinfo
 clear
 logo
-setupEnv
+#setupEnv
 clear
 logo
 setupFastfetch
@@ -665,7 +665,7 @@ echo -ne "
 touch /etc/sddm.conf
 touch /etc/sddm.conf.d/10-wayland.conf
 
-pacman -S --noconfirm --needed bash-completion nfs-utils usbutils nano ffmpeg btop gnome-keyring fuse pipewire pipewire-pulse pipewire-alsa starship fastfetch
+pacman -S --noconfirm --needed bash-completion nfs-utils usbutils nano ffmpeg btop gnome-keyring fuse pipewire pipewire-pulse pipewire-alsa starship fastfetch networkmanager-vpn-plugin-openvpn
 pacman -S --noconfirm --needed pavucontrol sddm kdeconnect flatpak
 pacman -S --noconfirm --needed steam gamescope prismlauncher gimp blender
 pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd noto-fonts-emoji qt5ct qt6ct
